d060fef1c76b57557574cba52b1af45502d2b5b7
admin <admin@DESKTOP-QRSKGK8>
2019-02-21 10:36:05 +0800

    捕获事件测试学习

--- springboot-activiti-web/src/test/java/com/zzj/it/SignalCatchEventTaskTest.java
+++ springboot-activiti-web/src/test/java/com/zzj/it/SignalCatchEventTaskTest.java
@@ -1,7 +1,5 @@
 package com.zzj.it;
 
-import static org.junit.Assert.*;
-
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
@@ -9,14 +7,21 @@ import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.Execution;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
 public class SignalCatchEventTaskTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(ScopeTest.class);
+
 	@Test
 	public void test() {
 		ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
@@ -33,6 +38,18 @@ public class SignalCatchEventTaskTest {
 		logger.info("启动流程实例" + pd.getId());
 		ProcessInstance pi = runService.startProcessInstanceById(pd.getId());
 		logger.info(pi.getId());
+
+		// 获取执行流
+		Execution exe = runService.createExecutionQuery().processInstanceId(pi.getId()).onlyChildExecutions()
+				.singleResult();
+
+		logger.error("{}当前执行节点{}", pi.getId(), exe.getActivityId());
+		//在发送触发信号的时候引用的配置中貌似使用的是name属性中的值，以后再测试
+		runService.signalEventReceived("singalTest");
+		// 获取执行流
+		exe = runService.createExecutionQuery().processInstanceId(pi.getId()).onlyChildExecutions().singleResult();
+
+		logger.error("{}当前执行节点{}", pi.getId(), exe.getActivityId());
 	}
 
 }
