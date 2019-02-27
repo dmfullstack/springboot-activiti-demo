80244d2cf16f681fe361465944fad3cce56cacf2
admin <admin@DESKTOP-QRSKGK8>
2019-02-27 15:10:34 +0800

    添加函数定义规则

--- springboot-activiti-drolos-service/src/main/java/com/zzj/it/moudels/example/entity/User.java
+++ springboot-activiti-drolos-service/src/main/java/com/zzj/it/moudels/example/entity/User.java
@@ -11,12 +11,22 @@ public class User implements Serializable {
 
 	private String userName;
 
+	private int amount;
+
 	public String getUserName() {
 		return userName;
 	}
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
 	}
 
 }
---
+++ springboot-activiti-drools-web/src/test/java/com/zzj/it/example/GrammerExample01.java
@@ -0,0 +1,52 @@
+package com.zzj.it.example;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.api.io.ResourceType;
+import org.kie.internal.KnowledgeBase;
+import org.kie.internal.builder.KnowledgeBuilder;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.definition.KnowledgePackage;
+import org.kie.internal.io.ResourceFactory;
+import org.kie.internal.runtime.StatefulKnowledgeSession;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.zzj.it.Application;
+import com.zzj.it.moudels.example.entity.User;
+
+/**
+ * drools 规则引擎函数定义测试
+ * @author zhouzj
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class GrammerExample01 {
+
+	@Test
+	public void test() {
+		KnowledgeBuilder knowledgeBuilder=KnowledgeBuilderFactory.newKnowledgeBuilder();
+		
+		knowledgeBuilder.add(ResourceFactory.newClassPathResource("rules/grammer-example01.drl"), ResourceType.DRL);
+
+		Collection<KnowledgePackage> pkgs=knowledgeBuilder.getKnowledgePackages();
+		
+		KnowledgeBase kbase=knowledgeBuilder.newKnowledgeBase();
+		
+		kbase.addKnowledgePackages(pkgs);
+		StatefulKnowledgeSession ksession=kbase.newStatefulKnowledgeSession();
+		
+		
+        // go !
+        User user=new User();
+        user.setUserName("zhouzj");
+        user.setAmount(1000);
+        ksession.insert(user);//插入
+        ksession.fireAllRules();//执行规则
+        ksession.dispose();
+	}
+
+}
